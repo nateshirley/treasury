427898c8e8f956c0e12707dde7e3226e3d75cafa
Nathan Shirley <nshirley22@gmail.com>
2021-12-03 14:28:40 -0500

    mostly a fail

--- programs/treasury/src/lib.rs
+++ programs/treasury/src/lib.rs
@@ -1,8 +1,9 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 const TRANSFER_SEED: &[u8] = b"transfer";
+const EXECUTE_SEED: &[u8] = b"execute";
 
 //this is a spec for implementing a basic treasury contract on solana
 
@@ -10,6 +11,59 @@ const TRANSFER_SEED: &[u8] = b"transfer";
 pub mod treasury {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        Ok(())
+    }
+
+    pub fn execute_instruction(
+        ctx: Context<ExecuteInstruction>,
+        ix_program_id: Pubkey,
+        ix_metas: Vec<AcctMeta>,
+        ix_execution_data: Vec<u8>,
+        ix_account_key: Pubkey,
+    ) -> ProgramResult {
+        /*
+        what anchor does is take the pubkeys and deserialize them into account infos
+        so if i take an array of pubkeys, i can do that as well,
+        */
+
+        let mut metas: Vec<AccountMeta> = vec![];
+        for account in ix_metas.iter() {
+            metas.push(account.to_account_meta())
+        }
+
+        let accounts = metas.clone();
+
+        let (key, bump) = Pubkey::find_program_address(&[EXECUTE_SEED], ctx.program_id);
+        let seeds = &[&EXECUTE_SEED, &[bump][..]];
+
+        let built_ix = solana_program::instruction::Instruction {
+            program_id: ix_program_id,
+            accounts: metas,
+            data: ix_execution_data,
+        };
+        solana_program::program::invoke_signed(
+            &built_ix,
+            &[
+                ctx.accounts.from.clone(),
+                ctx.accounts.to.clone(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[seeds],
+        )?;
+
+        // solana_program::program::invoke_signed(
+        //     &solana_program::system_instruction::transfer(
+        //         ctx.accounts.from.key,
+        //         ctx.accounts.to.key,
+        //         50000,
+        //     ),
+        //     &[
+        //         ctx.accounts.from.clone(),
+        //         ctx.accounts.to.clone(),
+        //         ctx.accounts.system_program.to_account_info(),
+        //     ],
+        //     &[seeds],
+        // )?;
         Ok(())
     }
 
@@ -36,6 +90,51 @@ pub fn _forum_address() -> Pubkey {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct ExecuteInstruction<'info> {
+    #[account(mut)]
+    from: AccountInfo<'info>,
+    #[account(mut)]
+    to: AccountInfo<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(AnchorDeserialize, AnchorSerialize, Default, Clone)]
+pub struct InstructionData {
+    /// Pubkey of the instruction processor that executes this instruction
+    pub program_id: Pubkey,
+    /// Metadata for what accounts should be passed to the instruction processor
+    pub accounts: Vec<AcctMeta>,
+    /// Opaque data passed to the instruction processor
+    pub data: Vec<u8>,
+}
+
+#[derive(AnchorDeserialize, AnchorSerialize, Default, Clone)]
+pub struct AcctMeta {
+    pub pubkey: Pubkey,
+    pub is_signer: bool,
+    pub is_writable: bool,
+}
+
+impl InstructionData {
+    fn to_account_metas(&self) -> Vec<AccountMeta> {
+        let mut metas: Vec<AccountMeta> = vec![];
+        for account in self.accounts.iter() {
+            metas.push(account.to_account_meta())
+        }
+        metas
+    }
+}
+impl AcctMeta {
+    fn to_account_meta(&self) -> AccountMeta {
+        return AccountMeta {
+            pubkey: self.pubkey,
+            is_signer: self.is_signer,
+            is_writable: self.is_writable,
+        };
+    }
+}
 
 #[derive(Accounts)]
 #[instruction(transfer_bump: u8)]
--- tests/treasury.ts
+++ tests/treasury.ts
@@ -1,17 +1,53 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { Treasury } from '../target/types/treasury';
+import * as anchor from "@project-serum/anchor";
+import * as web3 from "@solana/web3.js";
+import { Program } from "@project-serum/anchor";
+import { Treasury } from "../target/types/treasury";
 
-describe('treasury', () => {
-
+describe("treasury", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Treasury as Program<Treasury>;
+  let executor = null;
 
-  it('Is initialized!', async () => {
+  it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.rpc.initialize({});
     console.log("Your transaction signature", tx);
+    let [_executor, bump] = await web3.PublicKey.findProgramAddress(
+      [anchor.utils.bytes.utf8.encode("execute")],
+      program.programId
+    );
+    executor = _executor;
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(
+        _executor,
+        5 * web3.LAMPORTS_PER_SOL
+      ),
+      "confirmed"
+    );
+  });
+
+  it("pass ix", async () => {
+    let receiver = web3.Keypair.generate();
+    console.log(executor.toBase58());
+
+    let ix = web3.SystemProgram.transfer({
+      fromPubkey: executor,
+      toPubkey: provider.wallet.publicKey,
+      lamports: 0.000005 * web3.LAMPORTS_PER_SOL,
+    });
+    console.log(ix);
+
+    let vec = [1, 2, 3, 4, 5];
+
+    await program.rpc.executeInstruction(ix.programId, ix.keys, ix.data, {
+      accounts: {
+        from: executor,
+        to: provider.wallet.publicKey,
+        systemProgram: web3.SystemProgram.programId,
+      },
+    });
   });
 });
