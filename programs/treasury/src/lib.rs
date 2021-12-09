55844764cd05b3888590c3b5538557d420ddccf9
Nathan Shirley <nshirley22@gmail.com>
2021-12-09 13:18:18 -0500

    shit actually works s/o armani

--- programs/treasury/src/lib.rs
+++ programs/treasury/src/lib.rs
@@ -1,183 +1,184 @@
+use anchor_lang::solana_program::instruction::Instruction;
 use anchor_lang::{prelude::*, solana_program};
+use std::convert::Into;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
-const TRANSFER_SEED: &[u8] = b"transfer";
-const EXECUTE_SEED: &[u8] = b"execute";
+const TREASURY_SEED: &[u8] = b"treasury";
+const GOVERNOR_SEED: &[u8] = b"governor";
 
-//this is a spec for implementing a basic treasury contract on solana
+//this is a spec for treasury governance on solana. specifically with point-based nft voting
 
 #[program]
 pub mod treasury {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn create_governor(ctx: Context<CreateGovernor>, governor_bump: u8) -> ProgramResult {
+        ctx.accounts.governor.points_supply = 10;
+        ctx.accounts.governor.bump = governor_bump;
         Ok(())
     }
 
-    pub fn execute_instruction(
-        ctx: Context<ExecuteInstruction>,
-        ix_program_id: Pubkey,
-        ix_metas: Vec<AcctMeta>,
-        ix_execution_data: Vec<u8>,
-        ix_account_key: Pubkey,
+    pub fn create_transaction(
+        ctx: Context<CreateTransaction>,
+        pid: Pubkey,
+        accs: Vec<TransactionAccount>,
+        data: Vec<u8>,
     ) -> ProgramResult {
-        /*
-        what anchor does is take the pubkeys and deserialize them into account infos
-        so if i take an array of pubkeys, i can do that as well,
-        */
+        ctx.accounts.governor.transactions = ctx.accounts.governor.transactions + 1;
 
-        let mut metas: Vec<AccountMeta> = vec![];
-        for account in ix_metas.iter() {
-            metas.push(account.to_account_meta())
+        let tx = &mut ctx.accounts.transaction;
+        tx.governor = ctx.accounts.governor.key();
+        tx.program_id = pid;
+        tx.accounts = accs;
+        tx.data = data;
+        tx.points = 9;
+        tx.did_execute = false;
+        tx.index = ctx.accounts.governor.transactions;
+
+        Ok(())
+    }
+
+    pub fn execute_transaction(
+        ctx: Context<ExecuteTransaction>,
+        treasury_bump: u8,
+    ) -> ProgramResult {
+        if ctx.accounts.transaction.points < 4 {
+            panic!();
         }
+        // Execute the transaction signed by the treasury.
+        let mut ix: Instruction = (&*ctx.accounts.transaction).into();
+        ix.accounts = ix
+            .accounts
+            .iter()
+            .map(|acc| {
+                let mut acc = acc.clone();
+                if &acc.pubkey == ctx.accounts.treasury.key {
+                    acc.is_signer = true;
+                }
+                acc
+            })
+            .collect();
 
-        let accounts = metas.clone();
+        let accounts = ctx.remaining_accounts;
+        let seeds = &[&TREASURY_SEED[..], &[treasury_bump]];
+        solana_program::program::invoke_signed(&ix, accounts, &[seeds])?;
 
-        let (key, bump) = Pubkey::find_program_address(&[EXECUTE_SEED], ctx.program_id);
-        let seeds = &[&EXECUTE_SEED, &[bump][..]];
+        // Burn the transaction to ensure one time use.
+        ctx.accounts.transaction.did_execute = true;
 
-        let built_ix = solana_program::instruction::Instruction {
-            program_id: ix_program_id,
-            accounts: metas,
-            data: ix_execution_data,
-        };
-        solana_program::program::invoke_signed(
-            &built_ix,
-            &[
-                ctx.accounts.from.clone(),
-                ctx.accounts.to.clone(),
-                ctx.accounts.system_program.to_account_info(),
-            ],
-            &[seeds],
-        )?;
-
-        // solana_program::program::invoke_signed(
-        //     &solana_program::system_instruction::transfer(
-        //         ctx.accounts.from.key,
-        //         ctx.accounts.to.key,
-        //         50000,
-        //     ),
-        //     &[
-        //         ctx.accounts.from.clone(),
-        //         ctx.accounts.to.clone(),
-        //         ctx.accounts.system_program.to_account_info(),
-        //     ],
-        //     &[seeds],
-        // )?;
         Ok(())
     }
 
-    pub fn queue_transfer(
-        ctx: Context<QueueTransfer>,
-        transfer_bump: u8,
-        recipient: Pubkey,
-        lamports: u64,
-    ) -> ProgramResult {
-        ctx.accounts.transfer.index = ctx.accounts.governing_body.transfers;
-        ctx.accounts.governing_body.transfers = ctx.accounts.governing_body.transfers + 1;
-        ctx.accounts.transfer.recipient = recipient;
-        ctx.accounts.transfer.lamports = lamports;
-        ctx.accounts.transfer.bump = transfer_bump;
+    pub fn empty(ctx: Context<Empty>) -> ProgramResult {
         Ok(())
     }
 }
 
-pub fn _forum_address() -> Pubkey {
-    let program_id = Pubkey::default();
-    let (address, bump) = Pubkey::find_program_address(&[TRANSFER_SEED], &program_id);
-    address
-}
-
 #[derive(Accounts)]
-pub struct Initialize {}
-
-#[derive(Accounts)]
-pub struct ExecuteInstruction<'info> {
-    #[account(mut)]
-    from: AccountInfo<'info>,
-    #[account(mut)]
-    to: AccountInfo<'info>,
+#[instruction(governor_bump: u8)]
+pub struct CreateGovernor<'info> {
+    creator: Signer<'info>,
+    #[account(
+        init,
+        seeds = [GOVERNOR_SEED],
+        bump = governor_bump,
+        payer = creator
+    )]
+    governor: Account<'info, Governor>,
     system_program: Program<'info, System>,
 }
-
-#[derive(AnchorDeserialize, AnchorSerialize, Default, Clone)]
-pub struct InstructionData {
-    /// Pubkey of the instruction processor that executes this instruction
-    pub program_id: Pubkey,
-    /// Metadata for what accounts should be passed to the instruction processor
-    pub accounts: Vec<AcctMeta>,
-    /// Opaque data passed to the instruction processor
-    pub data: Vec<u8>,
+#[account]
+#[derive(Default)]
+pub struct Governor {
+    participants: u64,
+    points_supply: u64,
+    transactions: u32,
+    bump: u8,
 }
 
-#[derive(AnchorDeserialize, AnchorSerialize, Default, Clone)]
-pub struct AcctMeta {
+#[derive(Accounts)]
+pub struct CreateTransaction<'info> {
+    creator: Signer<'info>,
+    #[account(zero)]
+    transaction: Account<'info, Transaction>,
+    #[account(mut)]
+    governor: Account<'info, Governor>,
+}
+
+#[derive(Accounts)]
+#[instruction(treasury_bump: u8)]
+pub struct ExecuteTransaction<'info> {
+    #[account(mut)]
+    transaction: Account<'info, Transaction>,
+    governor: Account<'info, Governor>,
+    #[account(
+        mut,
+        seeds = [TREASURY_SEED],
+        bump = treasury_bump,
+    )]
+    treasury: AccountInfo<'info>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct Transaction {
+    // The multisig account this transaction belongs to.
+    pub governor: Pubkey,
+    // Target program to execute against.
+    pub program_id: Pubkey,
+    // Accounts requried for the transaction.
+    pub accounts: Vec<TransactionAccount>,
+    // Instruction data for the transaction.
+    pub data: Vec<u8>,
+    // signers[index] is true iff multisig.owners[index] signed the transaction.
+    //pub signers: Vec<bool>,
+    pub points: u64,
+    // Boolean ensuring one time execution.
+    pub did_execute: bool,
+    // Owner set sequence number.
+    pub index: u32,
+}
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Default)]
+pub struct TransactionAccount {
     pub pubkey: Pubkey,
     pub is_signer: bool,
     pub is_writable: bool,
 }
 
-impl InstructionData {
-    fn to_account_metas(&self) -> Vec<AccountMeta> {
-        let mut metas: Vec<AccountMeta> = vec![];
-        for account in self.accounts.iter() {
-            metas.push(account.to_account_meta())
+impl From<&Transaction> for Instruction {
+    fn from(tx: &Transaction) -> Instruction {
+        Instruction {
+            program_id: tx.program_id,
+            accounts: tx.accounts.iter().map(AccountMeta::from).collect(),
+            data: tx.data.clone(),
         }
-        metas
     }
 }
-impl AcctMeta {
-    fn to_account_meta(&self) -> AccountMeta {
-        return AccountMeta {
-            pubkey: self.pubkey,
-            is_signer: self.is_signer,
-            is_writable: self.is_writable,
-        };
+impl From<&TransactionAccount> for AccountMeta {
+    fn from(account: &TransactionAccount) -> AccountMeta {
+        match account.is_writable {
+            false => AccountMeta::new_readonly(account.pubkey, account.is_signer),
+            true => AccountMeta::new(account.pubkey, account.is_signer),
+        }
+    }
+}
+impl From<&AccountMeta> for TransactionAccount {
+    fn from(account_meta: &AccountMeta) -> TransactionAccount {
+        TransactionAccount {
+            pubkey: account_meta.pubkey,
+            is_signer: account_meta.is_signer,
+            is_writable: account_meta.is_writable,
+        }
     }
 }
 
 #[derive(Accounts)]
-#[instruction(transfer_bump: u8)]
-pub struct QueueTransfer<'info> {
-    initializer: Signer<'info>,
-    #[account(
-        init,
-        seeds = [TRANSFER_SEED],
-        bump = transfer_bump,
-        payer = initializer,
-    )]
-    transfer: Account<'info, Transfer>,
-    #[account(
-        constraint = forum.key() == _forum_address()
-    )]
-    forum: AccountInfo<'info>,
-    governing_body: Account<'info, GoverningBody>,
-    system_program: Program<'info, System>,
-}
-
-#[account]
-#[derive(Default)]
-pub struct Transfer {
-    index: u64,
-    recipient: Pubkey,
-    lamports: u64,
-    votes: u64,
-    bump: u8,
-}
-
-#[account]
-pub struct GoverningBody {
-    transfers: u64,
-    participants: u64,
-    points_supply: u64,
-}
-
+pub struct Empty {}
 /*
 
 so you could have something custom for allocating points and making transfers
-
 key ingredients
-
 
 
 some things i need to think a lot more about
--- tests/treasury.ts
+++ tests/treasury.ts
@@ -1,7 +1,13 @@
 import * as anchor from "@project-serum/anchor";
 import * as web3 from "@solana/web3.js";
+import { Keypair, SystemProgram, SYSVAR_RENT_PUBKEY } from "@solana/web3.js";
 import { Program } from "@project-serum/anchor";
 import { Treasury } from "../target/types/treasury";
+import { TOKEN_PROGRAM_ID, Token, MintLayout } from "@solana/spl-token";
+import {
+  createAssociatedTokenAccountInstruction,
+  getAssociatedTokenAccountAddress,
+} from "./helpers/tokenHelpers";
 
 describe("treasury", () => {
   // Configure the client to use the local cluster.
@@ -9,45 +15,233 @@ describe("treasury", () => {
   anchor.setProvider(provider);
 
   const program = anchor.workspace.Treasury as Program<Treasury>;
-  let executor = null;
+  let governor = null;
+  let governorBump = null;
+  let treasury = null;
+  let treasuryBump = null;
+  let transaction = web3.Keypair.generate();
+  let ix = null;
+  let mint = web3.Keypair.generate();
+  let treasuryTokenAccount = null;
+  let userTokenAccount = null;
+  let mintAuthority = Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
-    let [_executor, bump] = await web3.PublicKey.findProgramAddress(
-      [anchor.utils.bytes.utf8.encode("execute")],
+  it("config", async () => {
+    let [_governor, _governorBump] = await web3.PublicKey.findProgramAddress(
+      [anchor.utils.bytes.utf8.encode("governor")],
       program.programId
     );
-    executor = _executor;
+    governor = _governor;
+    governorBump = _governorBump;
+
+    let [_treasury, _treasuryBump] = await web3.PublicKey.findProgramAddress(
+      [anchor.utils.bytes.utf8.encode("treasury")],
+      program.programId
+    );
+    treasury = _treasury;
+    treasuryBump = _treasuryBump;
     await provider.connection.confirmTransaction(
       await provider.connection.requestAirdrop(
-        _executor,
+        treasury,
         5 * web3.LAMPORTS_PER_SOL
       ),
       "confirmed"
     );
-  });
-
-  it("pass ix", async () => {
-    let receiver = web3.Keypair.generate();
-    console.log(executor.toBase58());
-
-    let ix = web3.SystemProgram.transfer({
-      fromPubkey: executor,
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(
+        mintAuthority.publicKey,
+        5 * web3.LAMPORTS_PER_SOL
+      ),
+      "confirmed"
+    );
+    ix = web3.SystemProgram.transfer({
+      fromPubkey: treasury,
       toPubkey: provider.wallet.publicKey,
       lamports: 0.000005 * web3.LAMPORTS_PER_SOL,
     });
-    console.log(ix);
 
-    let vec = [1, 2, 3, 4, 5];
+    treasuryTokenAccount = await getAssociatedTokenAccountAddress(
+      treasury,
+      mint.publicKey
+    );
 
-    await program.rpc.executeInstruction(ix.programId, ix.keys, ix.data, {
+    userTokenAccount = await getAssociatedTokenAccountAddress(
+      provider.wallet.publicKey,
+      mint.publicKey
+    );
+  });
+
+  it("create governor", async () => {
+    const tx = await program.rpc.createGovernor(governorBump, {
       accounts: {
-        from: executor,
-        to: provider.wallet.publicKey,
+        creator: provider.wallet.publicKey,
+        governor: governor,
         systemProgram: web3.SystemProgram.programId,
       },
     });
+    console.log("create gov sig", tx);
+  });
+
+  //simple transfer out of treasury
+  it("create transaction", async () => {
+    let pid = ix.programId;
+    let accs = ix.keys;
+    let data = ix.data;
+
+    const tx = await program.rpc.createTransaction(pid, accs, data, {
+      accounts: {
+        creator: provider.wallet.publicKey,
+        transaction: transaction.publicKey,
+        governor: governor,
+      },
+      instructions: [
+        await program.account.transaction.createInstruction(transaction, 1000),
+      ],
+      signers: [transaction],
+    });
+
+    let queuedTx = await program.account.transaction.fetch(
+      transaction.publicKey
+    );
+  });
+  //execute lamps transfer
+  it("execute transaction", async () => {
+    const tx = await program.rpc.executeTransaction(treasuryBump, {
+      accounts: {
+        transaction: transaction.publicKey,
+        governor: governor,
+        treasury: treasury,
+      },
+      remainingAccounts: ix.keys
+        .map((meta: any) =>
+          meta.pubkey.equals(treasury) ? { ...meta, isSigner: false } : meta
+        )
+        .concat({
+          pubkey: web3.SystemProgram.programId,
+          isWritable: false,
+          isSigner: false,
+        }),
+    });
+  });
+
+  //gonna try a token account
+  it("token config", async () => {
+    let payer = provider.wallet.publicKey;
+
+    const tx = await program.rpc.empty({
+      accounts: {},
+      instructions: [
+        web3.SystemProgram.createAccount({
+          fromPubkey: payer,
+          newAccountPubkey: mint.publicKey,
+          space: MintLayout.span,
+          lamports: await provider.connection.getMinimumBalanceForRentExemption(
+            MintLayout.span
+          ),
+          programId: TOKEN_PROGRAM_ID,
+        }),
+        //init the mint
+        Token.createInitMintInstruction(
+          TOKEN_PROGRAM_ID,
+          mint.publicKey,
+          0,
+          mintAuthority.publicKey,
+          mintAuthority.publicKey
+        ),
+        //create token account for new member card
+        createAssociatedTokenAccountInstruction(
+          mint.publicKey,
+          treasuryTokenAccount,
+          treasury,
+          payer
+        ),
+        //create token account for user
+        createAssociatedTokenAccountInstruction(
+          mint.publicKey,
+          userTokenAccount,
+          payer,
+          payer
+        ),
+      ],
+      signers: [mint],
+    });
+  });
+
+  it("create treasury token transfer ix", async () => {
+    let transaction = web3.Keypair.generate();
+
+    let NewToken = new Token(
+      provider.connection,
+      mint.publicKey,
+      TOKEN_PROGRAM_ID,
+      mintAuthority
+    );
+    await NewToken.mintTo(treasuryTokenAccount, mintAuthority, [], 100);
+
+    let ix = Token.createTransferInstruction(
+      TOKEN_PROGRAM_ID,
+      treasuryTokenAccount,
+      userTokenAccount,
+      treasury,
+      [],
+      50
+    );
+    let pid = ix.programId;
+    let accs = ix.keys;
+    let data = ix.data;
+
+    const tx = await program.rpc.createTransaction(pid, accs, data, {
+      accounts: {
+        creator: provider.wallet.publicKey,
+        transaction: transaction.publicKey,
+        governor: governor,
+      },
+      instructions: [
+        await program.account.transaction.createInstruction(transaction, 1000),
+      ],
+      signers: [transaction],
+    });
+
+    let queuedTx = await program.account.transaction.fetch(
+      transaction.publicKey
+    );
+
+    let trezBalance = await provider.connection.getTokenAccountBalance(
+      treasuryTokenAccount
+    );
+    console.log("trez balance: ", trezBalance.value.uiAmount);
+    let userBalance = await provider.connection.getTokenAccountBalance(
+      userTokenAccount
+    );
+    console.log("user balance: ", userBalance.value.uiAmount);
+    //let transferIx = Token.createTransferInstruction(TOKEN_PROGRAM_ID);
+
+    const transferTx = await program.rpc.executeTransaction(treasuryBump, {
+      accounts: {
+        transaction: transaction.publicKey,
+        governor: governor,
+        treasury: treasury,
+      },
+      remainingAccounts: ix.keys
+        .map((meta: any) =>
+          meta.pubkey.equals(treasury) ? { ...meta, isSigner: false } : meta
+        )
+        .concat({
+          pubkey: TOKEN_PROGRAM_ID,
+          isWritable: false,
+          isSigner: false,
+        }),
+    });
+
+    //always append the programID, pass in accounts via remainingAccounts
+
+    let trezBalance2 = await provider.connection.getTokenAccountBalance(
+      treasuryTokenAccount
+    );
+    console.log("trez balance: ", trezBalance2.value.uiAmount);
+    let userBalance2 = await provider.connection.getTokenAccountBalance(
+      userTokenAccount
+    );
+    console.log("user balance: ", userBalance2.value.uiAmount);
   });
 });
